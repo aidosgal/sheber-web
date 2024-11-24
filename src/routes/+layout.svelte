85c8e3ea00b7895e2d7f379f8cb8e5dc9042dc9d
aidosgal <agalimzhan928@gmail.com>
2024-11-24 19:55:23 +0500

    feat: inital commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "../app.css";
+</script>
+
+<div class="py-5 flex">
+    <div class="w-[1000px] mx-auto">
+        <div class="flex"></div>
+        <slot />
+    </div>
+</div>
